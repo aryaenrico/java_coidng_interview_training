cdccd04ecd930145b284827f5b08fc1dfd9b48ba
Arya Enrico <aryaenrico789@gmail.com>
2023-10-06 00:50:51 +0700

    change approach to get o(N) time complexity

--- src/com/aryaEnrico/Main.java
+++ src/com/aryaEnrico/Main.java
@@ -1,10 +1,14 @@
 package com.aryaEnrico;
 
+import java.util.Arrays;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	QuestionTwoSum questionTwoSum   = new QuestionTwoSum ();
-    questionTwoSum.findTarget(new int[]{1,3,7,9,2},11);
+    int[] result = questionTwoSum.findTarget(new int[]{1,3,7,9,2},11);
+    System.out.println(Arrays.toString(result));
 
     }
 }
--- src/com/aryaEnrico/QuestionTwoSum.java
+++ src/com/aryaEnrico/QuestionTwoSum.java
@@ -1,25 +1,23 @@
 package com.aryaEnrico;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QuestionTwoSum {
     public int[] findTarget(int[] nums, int target) {
-        int pointer1, pointer2, numberToFind;
-        if (nums.length <= 1) {
-            return null;
-        }
+        int numberToFind;
+        Map<Integer, Integer> dictionary = new HashMap<Integer, Integer>();
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            pointer1 = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                pointer2 = nums[j];
-                numberToFind = target - pointer1;
-                if (pointer2 == numberToFind) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+            numberToFind = target - nums[i];
+            if (dictionary.get(numberToFind) != null) {
+                result[0] = i;
+                result[1] = dictionary.get(numberToFind);
+                return result;
+            }else{
+                dictionary.put(nums[i],i);
             }
         }
-
         return null;
     }
 }
